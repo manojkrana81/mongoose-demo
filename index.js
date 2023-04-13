dd18e7c91579f197317e70f7aab9e2a8609a0c2d
Manoj Kumar <manoj.kumar114@ad.infosys.com>
2023-04-13 08:58:41 +0530

    adding initial changes

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const usersRoute = require('./routes/users');
+const dotenv = require('dotenv');
+
+
+
+dotenv.config();
+const port = process.env.PORT || 8080
+
+// Connect to database
+async function connectToDatabase(){
+  await mongoose.connect(process.env.MONGODB_URL);
+}
+
+connectToDatabase().then(res => {
+  console.log('Database connected');
+}).
+  catch(err => {
+    console.log('Error occured connecting database');
+  });
+
+
+// Router middleware
+app.use('/', usersRoute);
+
+// Run server
+app.listen(port, ()=>{
+  console.log("Server started");
+})
